afa344df39a8199bf18bc1b65b22047bb68eef75
GinaKutz <gjkutz@gmail.com>
2023-12-13 15:37:04 -0600

    Added quiz questions and added alert at end

--- assets/script.js
+++ assets/script.js
@@ -6,11 +6,61 @@ const timerElement = document.getElementById('timer');
 const quizData = [
   // Add your quiz questions here as objects
   {
-    question: 'What is a string',
-    answers: ['a sequence of characters', 'a boolean', 'a sum of numbers', 'an image'],
-    correctAnswer: '1'
+    question: 'What does HTML stand for?',
+    answers: ['Hyperlinks and Text Markup Language', 'Hyper Text Markup Language', 'Home Tool Markup Language', 'Hyperlinking Text Markup Language'],
+    correctAnswer: 'Hyper Text Markup Language'
   },
-  // Add more questions similarly
+
+  {
+    question: 'Which of the following is not a programming language?',
+    answers: ['Python', 'CSS', 'Java', 'Ruby'],
+    correctAnswer: 'CSS'
+    
+  
+  },
+
+
+  {
+    question: 'What is the result of 5 + "5" in JavaScript?',
+    answers: ['10', '55', '"10"', 'Error'],
+    correctAnswer: '"10"'
+  },
+
+
+  {
+    question: 'What does CSS stand for?',
+    answers: ['Creative Style Sheets', 'Cascading Style Sheets', 'Computer Style Sheets', 'Colorful Style Sheets'],
+    correctAnswer: 'Cascading Style Sheets'
+  },
+
+
+  {
+    question: 'In Python, what is used to indicate a block of code following a conditional statement or loop?',
+    answers: ['Parenthesis', 'Curly Brackets', 'Indentation', 'Quotation Marks'],
+    correctAnswer: 'Indentation'
+  },
+
+
+  {
+    question: 'Which symbol is used for a single-line comment in most programming languages?',
+    answers: ['//', '#', '--', '/**/'],
+    correctAnswer: '//'
+  },
+
+
+  {
+    question: 'What is the purpose of the "git" version control system?',
+    answers: ['To write and execute code', 'To manage and track changes in files', 'To design user interface', 'To optimize website loading speed'],
+    correctAnswer: 'To manage and track changes in files'
+  },
+
+  {
+    question: 'What does the acronym API stand for in programming?',
+    answers: ['Application Programming Interface', 'Advanced Programming Interface', 'Automated Program Integration', 'All Purpose Interface'],
+    correctAnswer: 'Application Programming Interface'
+  },
+
+
 ];
 
 let currentQuestionIndex = 0;
@@ -39,13 +89,18 @@ function displayQuestion() {
 function checkAnswer(answer) {
   const currentQuestion = quizData[currentQuestionIndex];
   if (answer === currentQuestion.correctAnswer) {
-    // Handle correct answer
+
+    
     // Move to the next question
     currentQuestionIndex++;
     if (currentQuestionIndex < quizData.length) {
       displayQuestion();
     } else {
-      endQuiz();
+      function stopTimer(){
+        
+      }
+
+      alert('Good job! Quiz Complete! Insert your name and score in time remaining!');
     }
   } else {
     // Handle incorrect answer
@@ -58,16 +113,22 @@ function updateTimer() {
     timeLeft--;
     timerElement.textContent = timeLeft;
   } else {
-    endQuiz();
+    alert('Sorry, you have run out of time!');
   }
 }
 
 function endQuiz() {
   clearInterval(timer);
-  // Implement functionality to save initials and score
+
+   // Implement functionality to save initials and score
   // You can prompt the user for initials and store their score
   // Example: const initials = prompt('Enter your initials:');
   // Save the score somewhere or display it
+
+  const userName = nameInput.value || "User";
+  const timeRemaining = seconds.value || "Score";
+
+ 
 }
 
 startBtn.addEventListener('click', startQuiz);
